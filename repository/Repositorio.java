4708e2a7a75992f520ec5edce4fdfd11837e51a2
Sandra Sabrina Borges <sandrasabrinaborges@outlook.com>
2025-11-27 09:34:46 -0300

    Update Repositorio.java

--- repository/Repositorio.java
+++ repository/Repositorio.java
@@ -3,13 +3,34 @@ package repository;
 import java.util.List;
 
 /**
- * Contrato genérico para repositórios.
+ * Interface genérica que define operações básicas para repositórios de entidades.
  *
- * @param <T> tipo de entidade armazenada
+ * @param <T> Tipo de entidade gerenciada pelo repositório
  */
 public interface Repositorio<T> {
+
+    /**
+     * Adiciona um novo item ao repositório.
+     * @param item entidade a ser cadastrada
+     */
     void cadastrar(T item);
+
+    /**
+     * Busca um item pelo identificador único.
+     * @param id identificador do item
+     * @return item correspondente ou null se não encontrado
+     */
     T buscar(String id);
+
+    /**
+     * Remove um item, caso exista, com o ID fornecido.
+     * @param id identificador do item a ser removido
+     */
     void remover(String id);
+
+    /**
+     * Retorna uma lista com todos os itens cadastrados.
+     * @return lista de todos os itens
+     */
     List<T> listarTodos();
 }
