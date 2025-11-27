284f03b4eaec57335e9fa382b01dad53022a9b59
Sandra Sabrina Borges <sandrasabrinaborges@outlook.com>
2025-11-27 09:31:58 -0300

    Update InfoProd.java

--- model/InfoProd.java
+++ model/InfoProd.java
@@ -3,11 +3,11 @@ package model;
 import java.lang.annotation.*;
 
 /**
- * Anotação para marcar metadados de classes/atributos relacionados a produtos.
+ * Anotação utilizada para fornecer metadados a classes ou atributos de produtos.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD})
 public @interface InfoProd {
-    String versao() default "1.0";
-    String categoria() default "Geral";
+    String versao() default "1.0";           // Versão do produto ou classe
+    String categoria() default "Geral";      // Categoria padrão
 }
