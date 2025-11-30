90ae710251f3a278108bb46c93c577de7944071e
Sandra Sabrina Borges <sandrasabrinaborges@outlook.com>
2025-11-29 22:40:12 -0300

    Update and rename NegocioException.java to ExcecaoNegocio.java

--- exception/ExcecaoNegocio.java
+++ exception/ExcecaoNegocio.java
@@ -3,7 +3,7 @@ package exception;
 /**
  * Exceção personalizada lançada ao violar regras de negócio no sistema.
  */
-public class NegocioException extends Exception {
+public class ExcecaoNegocio extends Exception {
 
     private static final long serialVersionUID = 1L;
 
@@ -11,7 +11,7 @@ public class NegocioException extends Exception {
      * Cria uma exceção representando erro de negócio.
      * @param mensagem mensagem detalhada explicando o motivo do erro
      */
-    public NegocioException(String mensagem) {
+    public ExcecaoNegocio(String mensagem) {
         super(mensagem);
     }
 }
