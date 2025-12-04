15063a7729739167ffd27bf09ccae7d4c0a74bc8
Sandra Sabrina Borges <sandrasabrinaborges@outlook.com>
2025-12-04 11:27:46 -0300

    Update ExcecaoNegocio.java

--- exception/ExcecaoNegocio.java
+++ exception/ExcecaoNegocio.java
@@ -4,8 +4,8 @@ package exception;
  * Exceção personalizada lançada ao violar regras de negócio no sistema.
  */
 public class ExcecaoNegocio extends Exception {
-
-    private static final long serialVersionUID = 1L;
+    //Definition: The serialVersionUID is a universal version identifier for a Serializable class. Deserialization uses this number to ensure that a loaded class corresponds exactly to a serialized object. If no match is found, then an InvalidClassException is thrown.
+    private static final long serialVersionUID = 1L; 
 
     /**
      * Cria uma exceção representando erro de negócio.
